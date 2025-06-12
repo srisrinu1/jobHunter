6c8897e74805d7fccd6c332a4890bcecb7053e03
srinivasa reddy challa <srinivasreddychalla133@gmail.com>
2025-06-12 02:15:35 -0400

    feat(model): update refreshToken schema with indexes

---
+++ server/api/v1/models/refreshTokenModel.js
@@ -0,0 +1,37 @@
+const mongoose= require('mongoose');
+
+const refreshTokenSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    index:true,
+  },
+  token: {
+    type: String,
+    required: true,
+    unique: true,
+    index:true,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+    index:{expires:0},
+  },
+  blacklisted: {
+    type: Boolean,
+    default: false,
+    index:true,
+  },
+  userAgent: {
+    type: String,
+    default: '',
+  },
+  ip:{
+    type: String,
+    default: '',
+  },
+}, { timestamps: true });
+
+refreshTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+module.exports = mongoose.model('RefreshToken', refreshTokenSchema);
