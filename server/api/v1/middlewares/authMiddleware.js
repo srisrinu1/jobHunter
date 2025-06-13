53fef52d41d6798d573551efefa269e18585d448
srinivasa reddy challa <srinivasreddychalla133@gmail.com>
2025-06-13 02:14:59 -0400

    feat(auth): Implement JWT authentication and role-based authorization middlewares

---
+++ server/api/v1/middlewares/authMiddleware.js
@@ -0,0 +1,38 @@
+const passport=require('@config/passport');
+const STATUS=require('@config/statusCodes');
+
+const authenticateJWT = (req, res, next) => {
+    passport.authenticate('jwt', { session: false }, (err, user, info) => {
+        if (err) {
+        return res.status(STATUS.INTERNAL_SERVER_ERROR).json({ message: 'Internal server error' });
+        }
+        if (!user) {
+        return res.status(STATUS.UNAUTHORIZED).json({
+            success:false,
+             message: 'Unauthorized' });
+        }
+        req.user = user;
+        next();
+    })(req, res, next);
+    }
+
+const authorizeRoles = (...roles) => (req, res, next) => {
+    if(!req.user){
+        return res.status(STATUS.UNAUTHORIZED).json({
+            success: false,
+            message: 'Unauthorized: No user authenticated',
+        });
+    }
+    if (!roles.includes(req.user.role)) {
+        return res.status(STATUS.FORBIDDEN).json({
+            success: false,
+            message: 'Forbidden: You do not have permission to access this resource'
+        });
+    }
+    next();
+};
+
+module.exports = {
+    authenticateJWT,
+    authorizeRoles
+};
